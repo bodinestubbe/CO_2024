70721cca6e324329ad7995c529bc074d3f17b672
bodinestubbe <dinodientje@gmail.com>
2024-04-16 16:38:16 +0200

    savings

--- VRPtruckSavings.py
+++ VRPtruckSavings.py
@@ -5,38 +5,74 @@ import numpy as np
 
 class Truck:
 
-    def __init__(self, capacity, max_km, request):
+    def __init__(self, capacity, max_km):
         self.capacity = capacity
         self.max_km = max_km
-        self.route = [1, request.customerLocID,1]
+        self.route = []
         self.current_load = 0
         self.current_km = 0
-        self.day = 0
+        self.largestFromDate = 0
+        self.smallestToDate = 0
+        self.deliveryDay = 0
 
 
-def generate_savings(distances):
-    savings = []
-    for i in range(1,len(distances)):
-        for j in range(1,len(distances)):
-            savings.append(((i,j),distances[i][0] + distances[0][j] - distances[i][j]))
+def generate_savings(requests, distances):
+    savings = {}
+    for i in range(0,len(requests)):
+        for j in range(0,len(requests)):
+            if i != j:
+                request_1 = requests[i].ID
+                request_2 = requests[j].ID
+
+                Loc1 = requests[i].customerLocID
+                Loc2 = requests[j].customerLocID
+
+                if (request_2, request_1) not in savings:
+                    savings[request_1,request_2] = distances[Loc1-1][0] + distances[0][Loc2-1] - distances[Loc1-1][Loc2-1]
+                
     
-    return sorted(savings, key = lambda x:x[1], reverse = True)
+    return {k: v for k, v in sorted(savings.items(), key=lambda item: item[1], reverse = True)}
 
-def canMerge():
+def canMerge(reqIDS):
+    reqID1 = reqIDS[0]
+    reqID2 = reqIDS[1]
+
     # do all requests in route have overlap in delivery time
+
+
+    # is it in a route
     # is it still within capacity
+    
     # has max km not been surpassed
 
     return None
 
+
+def hasOverlap(reqID1, reqID2):
+    return reqID1.toDay >= reqID2.fromDay and reqID2.toDay >= reqID1.fromDay
+
 def generate_feasible_truck_tour(instance):
     planning_horizon = range(1, instance.days + 1)
     distance_matrix = instance.distances
-    savings = generate_savings(distance_matrix)
-
-    trucks = [Truck(instance.truckCapacity, instance.truckMaxDistance, request) for request in instance.requests]
+    savings = generate_savings(instance.Requests, distance_matrix)
+    print(savings)
+    trucks = [Truck(instance.maxCapacity, instance.maxDistance) for request in instance.Requests]
     
-    for saving in savings:
+    for reqIDs, value in savings.items():
+        
+    
+        reqID1 = reqIDs[0]
+        reqID2 = reqIDs[1]
+
+        # do all requests in route have overlap in delivery time
+        if hasOverlap(reqID1):
+
+
+        # is it in a route
+        # is it still within capacity
+        
+        # has max km not been surpassed
+
 
 
 
